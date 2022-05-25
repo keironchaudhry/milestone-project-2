f89f237bae1a16fe834e6d5132fa21b39f639887
Keiron Chaudhry <keironliamchaudhry@outlook.com>
2022-05-25 07:16:43 +0000

    Begin javascript code for button response

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,7 @@
-console.log("Inicialised!")
+let buttons = document.getElementsByTagName("button");
+
+function output() {
+    buttons.addEventListener("click", output, true);
+    alert("You have clicked a button!");
+}
+
