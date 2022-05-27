cf763d8123fc0dcaf0d5153e50c5d752a95a4e79
Keiron Chaudhry <keironliamchaudhry@outlook.com>
2022-05-27 09:09:35 +0000

    Add alerts to onclick functions to check for functionality in DOM

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,52 +2,52 @@
 let playerScore = 0;
 let computerScore = 0;
 
-document.getElementById("rock-button");
-document.getElementById("paper-button");
-document.getElementById("scissors-button");
-document.getElementById("lizard-button");
-document.getElementById("spock-button");
+document.getElementById("rock-button").onclick=playerRock;
+document.getElementById("paper-button").onclick=playerPaper;
+document.getElementById("scissors-button").onclick=playerScissors;
+document.getElementById("lizard-button").onclick=playerLizard;
+document.getElementById("spock-button").onclick=playerSpock;
 
 /**
  * Function for player clicking "rock"
  */
 function playerRock() {
-
+    alert("You picked rock!");
 }
 
 /**
  * Function for player clicking "paper"
  */
 function playerPaper() {
-
+    alert("You picked paper!");
 }
 
 /**
  * Function for player clicking "scissors"
  */
 function playerScissors() {
-    
+    alert("You picked scissors!");
 }
 
 /**
  * Function for player clicking "lizard"
  */
 function playerLizard() {
-
+    alert("You picked lizard!");
 }
 
 /**
  * Function for player clicking "spock"
  */
 function playerSpock() {
-
+    alert("You picked spock!");
 }
 
 /**
  * Function for the basic AI to make a choice of hand
  */
 function randomChoice() {
-    
+
 
 }
 
