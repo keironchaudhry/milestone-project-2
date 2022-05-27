89a1a1593fe812953caf2cb2748580d082028312
Keiron Chaudhry <keironliamchaudhry@outlook.com>
2022-05-27 09:05:25 +0000

    Add onclick click events to html buttons in js and wire to functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,23 +8,66 @@ document.getElementById("scissors-button");
 document.getElementById("lizard-button");
 document.getElementById("spock-button");
 
+/**
+ * Function for player clicking "rock"
+ */
 function playerRock() {
 
 }
 
+/**
+ * Function for player clicking "paper"
+ */
 function playerPaper() {
 
 }
 
+/**
+ * Function for player clicking "scissors"
+ */
 function playerScissors() {
     
 }
 
+/**
+ * Function for player clicking "lizard"
+ */
 function playerLizard() {
 
 }
 
+/**
+ * Function for player clicking "spock"
+ */
 function playerSpock() {
 
 }
 
+/**
+ * Function for the basic AI to make a choice of hand
+ */
+function randomChoice() {
+    
+
+}
+
+/**
+ * Function for determining whether Player or AI has won
+ */
+function checkWinner() {
+
+}
+
+/**
+ * Function for increasing player score
+ */
+function incrementPlayerScore() {
+
+}
+
+/**
+ * Function for increasing AI score
+ */
+function incrementComputerScore() {
+
+}
